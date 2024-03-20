8ce08cb895eafe3c550d360145a2df759ded8890
mustachetrashstache <matthewbushgames@gmail.com>
2024-03-20 17:39:59 -0400

    Fixed BP code

--- orlando-permit-rework/src/App.js
+++ orlando-permit-rework/src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Henlo Wrld!</h1>
+        {/* Content of header */}
+        My Page Header
+      </header>
     </div>
   );
 }
